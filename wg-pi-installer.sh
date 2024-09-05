74f792682bd2c0a5b70aeac93e0e51008b9dfa4a
Coleganet <jacob@coleganet.com>
2024-09-05 19:32:20 +0200

    Update wg-pi-installer.sh

--- wg-pi-installer.sh
+++ wg-pi-installer.sh
@@ -420,7 +420,7 @@ function uninstallWg() {
 
 function manageMenu() {
 	echo "Welcome to WireGuard-install!"
-	echo "The git repository is available at: https://github.com/angristan/wireguard-install"
+	echo "The git repository is available at: https://github.com/Coleganet/wireguard_PI"
 	echo ""
 	echo "It looks like WireGuard is already installed."
 	echo ""
